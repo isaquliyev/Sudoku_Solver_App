218ba19b0cfe4e1d1c041c323ba0caa3ff26dc2d
isaquliyev <isa.quluyev.wip@gmail.com>
2026-06-04 16:25:32 +0200

    - .gitignore

--- opencv/build/intermediates/cxx/Debug/1s2h1u5w/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\HP\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\ComposeProjects\\sudoku_solver_ai\\opencv\\.cxx\\Debug\\1s2h1u5w\\arm64-v8a" ^
-  opencv_jni_shared
